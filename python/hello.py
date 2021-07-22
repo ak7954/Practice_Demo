ac0e6174da360874c460d42276f39a1f9ec47dcf
ak7954 <54507185+ak7954@users.noreply.github.com>
2021-07-22 20:07:21 +0530

    Create hello.py

---
+++ python/hello.py
@@ -0,0 +1,10 @@
+print('Hello..! I am practicing newly on git to turn my self as an advance')
+
+print("!!!!!!@@!!!!!!!!!!!@@!!!!@@")
+print("!!!!@@!@@!!!!!!!!!!@@!!@@")
+print("!!!@@@@@@@!!!!!!!!!@@!@@")
+print(!!!@@!!!!!@@!!!!!!!!@@!!@@@")
+print(!!@@!!!!!!!@@!!!!!!!@@!!!!@@@@@@@@@@@")
+
+      
+print("will move on...!")
